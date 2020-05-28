f6f693bd833c348d863d811049d0462418b38512
inhere <in.798@qq.com>
2020-05-28 18:05:06 +0800

    update some for run

--- app/Helper/SysCmd.php
+++ app/Helper/SysCmd.php
@@ -9,9 +9,19 @@
 
 namespace Inhere\Kite\Helper;
 
-use Toolkit\Cli\Color;
 use Swoole\Coroutine;
+use Toolkit\Cli\Color;
 use Toolkit\Sys\Sys;
+use function chdir;
+use function exec;
+use function function_exists;
+use function implode;
+use function ob_get_clean;
+use function ob_start;
+use function passthru;
+use function shell_exec;
+use function system;
+use function trim;
 
 /**
  * Class SysCmd
@@ -52,6 +62,63 @@ class SysCmd
             'code'   => $code,
             'output' => $output,
         ];
+    }
+
+    /**
+     * Method to execute a command in the sys
+     * Uses :
+     * 1. system
+     * 2. passthru
+     * 3. exec
+     * 4. shell_exec
+     *
+     * @param string      $command
+     * @param bool        $returnStatus
+     * @param string|null $cwd
+     *
+     * @return array|string
+     */
+    public static function exec2(string $command, bool $returnStatus = true, string $cwd = null)
+    {
+        $output   = '';
+        $exitCode = 1;
+
+        if ($cwd) {
+            chdir($cwd);
+        }
+
+        // system
+        if (function_exists('system')) {
+            ob_start();
+            system($command, $exitCode);
+            $output = ob_get_clean();
+
+            // passthru
+        } elseif (function_exists('passthru')) {
+            ob_start();
+            passthru($command, $exitCode);
+            $output = ob_get_clean();
+            // exec
+        } elseif (function_exists('exec')) {
+            exec($command, $output, $exitCode);
+            $output = implode("\n", $output);
+
+            // shell_exec
+        } elseif (function_exists('shell_exec')) {
+            $output = shell_exec($command);
+        } else {
+            $output   = 'Command execution not possible on this system';
+            $exitCode = 0;
+        }
+
+        if ($returnStatus) {
+            return [
+                'output' => trim($output),
+                'status' => $exitCode
+            ];
+        }
+
+        return trim($output);
     }
 
     /**
