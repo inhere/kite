e8b40ccf89d634347848b73247cdb960f2cd222b
inhere <in.798@qq.com>
2020-06-29 19:57:31 +0800

    update soe for env command

--- app/Console/Command/EnvCommand.php
+++ app/Console/Command/EnvCommand.php
@@ -12,6 +12,8 @@ namespace Inhere\Kite\Console\Command;
 use Inhere\Console\Command;
 use Inhere\Console\IO\Input;
 use Inhere\Console\IO\Output;
+use function is_scalar;
+use function strpos;
 
 /**
  * Class DemoCommand
@@ -26,7 +28,9 @@ class EnvCommand extends Command
      * print system ENV information
      *
      * @options
-     *  --format    Format the env value
+     *  --format        Format the env value
+     *  --split         Split the env value by given char. eg ':' ','
+     *  --match-value   Match ENV value by keywords. default is match key.
      *
      * @arguments
      *  keywords    The keywords for search ENV
@@ -38,24 +42,50 @@ class EnvCommand extends Command
     {
         $keywords = $input->getFirstArg();
 
-        if ($keywords) {
-            $upKey = strtoupper($keywords);
+        if (!$keywords) {
+            // env | grep XXX
+            $output->aList($_SERVER, 'ENV Information', ['ucFirst' => false]);
+            return;
+        }
+
+        $upKey = strtoupper($keywords);
+
+        if (isset($_SERVER[$upKey])) {
+            $value = $_SERVER[$upKey];
+            $char  = $input->getStringOpt('split');
 
             if ($upKey === 'PATH') {
-                $pathString = $_SERVER['PATH'] ?? '';
-
-                $output->aList(explode(':', $pathString), 'path value', ['ucFirst' => false]);
-                return;
+                $char = ':';
             }
 
-            if (isset($_SERVER[$upKey])) {
-                $output->colored($_SERVER[$upKey]);
+            if ($char) {
+                $output->aList(explode($char, $value), "$upKey value", ['ucFirst' => false]);
+            } else {
+                $output->colored($value);
+            }
 
-                return;
+            return;
+        }
+
+        $matched  = [];
+        $matchVal = $input->getBoolOpt('match-value');
+
+        foreach ($_SERVER as $key => $value) {
+            $hayStack = $matchVal ? $value : $key;
+            $needle   = $matchVal ? $keywords : $upKey;
+            if (!is_scalar($hayStack)) {
+                continue;
+            }
+
+            if (strpos((string)$hayStack, $needle) !== false) {
+                $matched[$key] = $value;
             }
         }
 
-        // env | grep XXX
-        $output->aList($_SERVER, 'ENV Information', ['ucFirst' => false]);
+        if (!$matched) {
+            $matched = ['NOT MATCHED'];
+        }
+
+        $output->aList($matched, "Matched Information(kw:{$keywords})", ['ucFirst' => false]);
     }
 }
