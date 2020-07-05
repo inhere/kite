f0856260fb6a1d34a0c963521871081bd91a88a3
inhere <in.798@qq.com>
2020-07-05 10:14:01 +0800

    upsome

--- app/Common/GitLocal/GitLocal.php
+++ app/Common/GitLocal/GitLocal.php
@@ -2,6 +2,8 @@
 
 namespace Inhere\Kite\Common\GitLocal;
 
+use Toolkit\Sys\Sys;
+
 /**
  * Class GitLocal
  *
@@ -29,10 +31,41 @@ class GitLocal extends AbstractGitLocal
     }
 
     /**
+     * find changed or new created files by git status.
+     * @throws \RuntimeException
+     */
+    public function findChangedFiles()
+    {
+        // -u expand dir's files
+        [, $output,] = Sys::run('git status -s -u', $this->workDir);
+
+        // 'D some.file'    deleted
+        // ' M some.file'   modified
+        // '?? some.file'   new file
+        foreach (explode("\n", trim($output)) as $file) {
+            $file = trim($file);
+
+            // only php file.
+            if (!strpos($file, '.php')) {
+                continue;
+            }
+
+            // modified files
+            if (strpos($file, 'M ') === 0) {
+                yield substr($file, 2);
+
+                // new files
+            } elseif (strpos($file, '?? ') === 0) {
+                yield substr($file, 3);
+            }
+        }
+    }
+
+    /**
      * Get the value of errCode
      *
      * @return int
-     */ 
+     */
     public function getErrCode(): int
     {
         return $this->errCode;
@@ -42,7 +75,7 @@ class GitLocal extends AbstractGitLocal
      * Get error message
      *
      * @return string
-     */ 
+     */
     public function getErrMsg(): string
     {
         return $this->errMsg;
