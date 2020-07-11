b91128292de44342b128153070452aec3e310118
inhere <in.798@qq.com>
2020-07-11 17:07:11 +0800

    add some new command

--- app/Console/Controller/GenerateController.php
+++ app/Console/Controller/GenerateController.php
@@ -5,6 +5,14 @@ namespace Inhere\Kite\Console\Controller;
 use Inhere\Console\Controller;
 use Inhere\Console\IO\Input;
 use Inhere\Console\IO\Output;
+use Toolkit\Stdlib\Str;
+use Toolkit\Sys\Proc\ProcWrapper;
+use function date;
+use function explode;
+use function file_get_contents;
+use function parse_ini_string;
+use function str_replace;
+use function trim;
 
 /**
  * Class GenerateGroup
@@ -22,6 +30,14 @@ class GenerateController extends Controller
         return ['generate'];
     }
 
+    protected static function commandAliases(): array
+    {
+        return [
+            'rpt' => 'repeat',
+            'tpl' => 'template',
+        ];
+    }
+
     /**
      * @param Input  $input
      * @param Output $output
@@ -29,5 +45,78 @@ class GenerateController extends Controller
     public function readmeCommand(Input $input, Output $output): void
     {
         $output->success('Complete');
+    }
+
+    /**
+     * @param Input $input
+     */
+    protected function templateConfigure(Input $input): void
+    {
+        $input->bindArgument('filename', 0);
+    }
+
+    /**
+     * Create an template file on runtime dir.
+     *
+     * @options
+     *  --editor    Editor for edit the template file
+     *
+     * @arguments
+     *  filename     The template filename. If not set, will auto generate by datetime.
+     *
+     * @param Input  $input
+     * @param Output $output
+     */
+    public function templateCommand(Input $input, Output $output): void
+    {
+        $editor   = $input->getStringOpt('editor', 'vim');
+        $filepath = 'runtime/' . date('Ymd_Hi') . '.tpl';
+
+        ProcWrapper::runEditor($editor, $filepath);
+
+        $output->writeln($filepath);
+    }
+
+    /**
+     * @param Input $input
+     */
+    protected function repeatConfigure(Input $input): void
+    {
+        $input->bindArgument('tpl', 0);
+    }
+
+    /**
+     * Repeat generate some codes by input vars
+     *
+     * @arguments
+     *  tpl         The template filepath
+     *
+     * @param Input  $input
+     * @param Output $output
+     */
+    public function repeatCommand(Input $input, Output $output): void
+    {
+        $tplFile = $input->getRequiredArg('tpl');
+
+        $content = file_get_contents($tplFile);
+
+        [$varDefine, $template] = explode('###', $content);
+
+        $vars = (array)parse_ini_string(trim($varDefine), true);
+
+        $template = trim($template);
+        $snippets = [''];
+
+        foreach ($vars as $var => $valString) {
+            $values = Str::explode($valString, ',');
+
+            // repeat by values
+            foreach ($values as $val) {
+                $snippets[] = str_replace('{$' . $var . '}', $val, $template);
+            }
+        }
+
+        $output->success('Complete');
+        $output->writeRaw(\implode("\n", $snippets));
     }
 }
--- app/Model/Logic/GitChangeLog.php
+++ app/Model/Logic/GitChangeLog.php
@@ -2,7 +2,35 @@
 
 namespace Inhere\Kite\Model\Logic;
 
+/**
+ * Class GitChangeLog
+ *
+ * @package Inhere\Kite\Model\Logic
+ */
 class GitChangeLog
 {
+    public static function new(): self
+    {
+        return new self();
+    }
 
+    public function parse(): self
+    {
+        return $this;
+    }
+
+    public function getChangeLog(): array
+    {
+        return [];
+    }
+
+    public function toArray(): array
+    {
+        return [];
+    }
+
+    public function export(): void
+    {
+
+    }
 }
