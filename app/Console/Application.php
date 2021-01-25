cc80470cdba2748288991e97a561cb0318738583
inhere <in.798@qq.com>
2021-01-25 20:16:06 +0800

    update some for not found handle

--- app/Console/Application.php
+++ app/Console/Application.php
@@ -9,6 +9,8 @@
 
 namespace Inhere\Kite\Console;
 
+use Inhere\Console\ConsoleEvent;
+use Inhere\Kite\Common\CmdRunner;
 use Inhere\Kite\Kite;
 use Toolkit\Stdlib\Arr\ArrayHelper;
 use function file_exists;
@@ -35,12 +37,14 @@ class Application extends \Inhere\Console\Application
         Kite::setCliApp($this);
 
         $this->loadAppConfig();
+
+        $this->on(ConsoleEvent::ON_NOT_FOUND, $this->onNotFound());
     }
 
     private function loadAppConfig(): void
     {
         $baseFile = BASE_PATH . '/config/config.php';
-        $loaded = [$baseFile];
+        $loaded   = [$baseFile];
 
         // 基础配置
         /** @noinspection PhpIncludeInspection */
@@ -69,5 +73,49 @@ class Application extends \Inhere\Console\Application
 
         $config['__loaded_file'] = $loaded;
         $this->setConfig($config);
+    }
+
+    protected function onNotFound(): callable
+    {
+        return static function (string $cmd, Application $app) {
+            $aliases = $app->getParam('aliases', []);
+
+            // is an command alias.
+            if ($aliases && isset($aliases[$cmd])) {
+                $realCmd = $aliases[$cmd];
+
+                $app->notice("input command is alias name, will redirect to the real command '$realCmd'");
+                $app->dispatch($realCmd);
+                return true;
+            }
+
+            // custom scripts
+            $scripts = $app->getParam('scripts', []);
+            if (!$scripts || !isset($scripts[$cmd])) {
+                // want call system command.
+                if ($cmd[0] === '\\') {
+                    $cmd = substr($cmd, 1);
+                }
+
+                $cmdLine = $app->getInput()->getFullScript();
+                $app->notice("input command is not found, will call system command: $cmdLine");
+
+                // call system command
+                CmdRunner::new($cmdLine)->do(true);
+                return true;
+            }
+
+            // redirect to run custom scripts.
+            /** @see \Inhere\Kite\Console\Command\RunCommand::execute() */
+            $app->note("command not found, redirect to run script: $cmd");
+
+            $args = $app->getInput()->getArgs();
+            $args = array_merge([$cmd], $args);
+
+            $app->getInput()->setArgs($args, true);
+            $app->dispatch('run');
+
+            return true;
+        };
     }
 }
