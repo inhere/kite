9308b4fd25142340a970a46b2f7c2abbbcee3823
inhere <in.798@qq.com>
2021-05-09 09:01:25 +0800

    update some config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,7 +8,7 @@ This file is part of Kite.
 @license  MIT
 EOF;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
