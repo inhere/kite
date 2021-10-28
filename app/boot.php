a85748edcde042f77fe29ab84365a2811810822b
Inhere <in.798@qq.com>
2021-10-28 10:59:18 +0800

    chore: update deps, re-gen composer lock

---
+++ app/boot.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE_PATH', dirname(__DIR__));
+
+/** @var Composer\Autoload\ClassLoader $loader */
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+
+$loader->addPsr4("Toolkit\\PFlag\\", __DIR__ . '/toolkit/pflag/src/');
+$loader->addPsr4("Symfony\\Component\\Yaml\\", __DIR__ . '/symfony/yaml/');
